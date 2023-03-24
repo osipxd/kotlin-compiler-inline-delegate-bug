24f079fe2e1129863fc0d4434c8fd27dbc6c6b5a
Osip Fatkullin <osip.fatkullin@gmail.com>
2023-03-24 17:29:48 +0300

    Update Kotlin to 1.8.10, enable IR fro KAPT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.20"
-    kotlin("kapt") version "1.5.20"
+    kotlin("jvm") version "1.8.10"
+    kotlin("kapt") version "1.8.10"
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
     implementation(project(":delegate-lib"))
 
     // Needed to reproduce the bug (condition 3)
-    kapt("com.google.dagger:dagger-compiler:2.37")
+    kapt("com.google.dagger:dagger-compiler:2.45")
 }
 
 allprojects {
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            useIR = true // IR is enabled
-            jvmTarget = "1.8"
-        }
-    }
-
     repositories {
         mavenCentral()
     }
